a799aaf4e8e1210444ca8aa5ebfe3e0dba3795b7
Mani Movassagh <mani.mgh@gmail.com>
2024-12-27 22:47:38 +0100

    worked perfect

--- frontend/services/keycloak.ts
+++ frontend/services/keycloak.ts
@@ -3,8 +3,8 @@ import Keycloak from 'keycloak-js';
 // Configure Keycloak instance
 const keycloak = new Keycloak({
   url: 'http://localhost:8081', // Keycloak server base URL
-  realm: 'my-realm',           // Your Keycloak realm name
-  clientId: 'my-nextjs-app',   // Your Keycloak client ID
+  realm: 'auth_solution',           // Your Keycloak realm name
+  clientId: 'auth_front',   // Your Keycloak client ID
 });
 
 export default keycloak;
